19897f1b8eb74c0861da5e8bf144e95600c0874a
Diego Guedes <diego@saneago.com.br>
2022-05-28 01:16:10 -0300

    finished the project with all tests passed

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# nodejs-solid-principle
+# NodeJS using solid principle
+
 Practicing SOLID principle in a NodeJS project
+
+# Run test
+
+yarn && yarn test
+
+# Run
+
+yarn && yarn dev
--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,14 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const user_id = request.header("user_id");
+    try {
+      const allUsers = this.listAllUsersUseCase.execute({user_id});
+      
+      return response.status(200).json(allUsers);      
+    } catch (error) {
+      return response.status(400).json({ error: "mensagem do erro" });
+    }
   }
 }
 
